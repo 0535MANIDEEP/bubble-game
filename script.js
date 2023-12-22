98824808d5de858c98bb9de75e8df78354248d64
Manideep <manideepdaram@gmail.com>
2023-12-22 12:01:23 +0530

    after adding the timing function

--- script.js
+++ script.js
@@ -1,10 +1,17 @@
 let clutter= "";
-
-for(let i=1;i<=500;i++)
+let score=0
+for(let i=1;i<=300;i++)
 {
     let num=Math.floor(Math.random()*10);
     console.log(num)
-    clutter +=`<div id=bubble>${num}</div>`;
+    clutter +=`<div id=bubble onClick="points(1)">${num}</div>`;
     
 }
 document.querySelector(".bottom-part").innerHTML=clutter;
+document.getElementById("bubble").addEventListener("click",points);
+
+function points(one){
+    score+=one;
+}
+
+console.log(score);
